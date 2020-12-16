21eeee9f96a35add3268d90003009e7f5c740ba7
Gabe <drago@DESKTOP-JH6S3Q5.localdomain>
2020-12-15 19:19:50 -0500

    Add rate limiter to login route using express-rate-limit

--- server/admin.js
+++ server/admin.js
@@ -1,5 +1,17 @@
 const app = require('./index.js');
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+const rateLimit = require('express-rate-limit');
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// // see https://expressjs.com/en/guide/behind-proxies.html
+// // app.set('trust proxy', 1);
+
+const apiLimiter = rateLimit({
+	windowMs: process.env.LOGIN_LIMIT_TIME_PERIOD || 15 * 1000,
+	max: process.env.LOGIN_REQUESTS_PER_PERIOD || 1
+});
+
+app.use('/api/login', apiLimiter);
 
 app.post('/api/login', function(req, res) {
   if (req.body.password === process.env.ADMIN_PW) {

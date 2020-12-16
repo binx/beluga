f074bc34c43fa2473de831276b5f43cbb70c5e39
gabekole <72774655+gabekole@users.noreply.github.com>
2020-12-16 15:21:40 -0500

    Allow possibility of no admin password

--- server/admin.js
+++ server/admin.js
@@ -7,29 +7,27 @@ const { parse, stringify } = require('envfile');
 const pathToenvFile = 'config.env';
 const saltRounds = 13;
 
-if ( !process.env.ADMIN_PW ){
-	throw 'ADMIN_PW must be provided in config.env';
-}
-
 if ( !process.env.PASSWORD_IS_HASHED || process.env.PASSWORD_IS_HASHED !== 'true' ){
-	bcrypt.hash(process.env.ADMIN_PW, saltRounds, function(err, hash) {
-		fs.readFile(pathToenvFile, {encoding:'utf8', flag:'r'}, function(err, data) {
-			if(err) {
-				console.log(err);
-			}
-			let result = parse(data);
-			result['ADMIN_PW'] = hash;
-			result['PASSWORD_IS_HASHED'] = 'true';
-			process.env['ADMIN_PW'] = hash;
-			process.env['PASSWORD_IS_HASHED'] = 'true';	
-
-			fs.writeFile(pathToenvFile, stringify(result), function (err) {
-				if (err) {
+	if ( process.env.ADMIN_PW ){
+		bcrypt.hash(process.env.ADMIN_PW, saltRounds, function(err, hash) {
+			fs.readFile(pathToenvFile, {encoding:'utf8', flag:'r'}, function(err, data) {
+				if(err) {
 					console.log(err);
 				}
+				let result = parse(data);
+				result['ADMIN_PW'] = hash;
+				result['PASSWORD_IS_HASHED'] = 'true';
+				process.env['ADMIN_PW'] = hash;
+				process.env['PASSWORD_IS_HASHED'] = 'true';	
+
+				fs.writeFile(pathToenvFile, stringify(result), function (err) {
+					if (err) {
+						console.log(err);
+					}
+				});
 			});
 		});
-	});
+	}
 }
 
 const apiLimiter = rateLimit({
@@ -41,7 +39,7 @@ app.use('/api/login', apiLimiter);
 
 app.post('/api/login', function(req, res) {
   bcrypt.compare(req.body.password, process.env.ADMIN_PW, (err, result) => {
-	if ( result === true ) {
+	if ( process.env.ADMIN_PW && result === true ) {
 		req.session.isAdmin = true;
 		res.json({ isAdmin: true })
 	} else {
